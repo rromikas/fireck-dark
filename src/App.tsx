3141c40f51f7e39b75e4d5771e067c29e090f57d
rromikas <rromikas@gmail.com>
2021-06-27 15:07:15 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { ReactComponent as Editor } from "assets/editor.svg";
 import Button from "components/Button";
 import { useEffect, useState } from "react";
 import Footer from "components/Footer";
+import { InView } from "react-intersection-observer";
 
 function App() {
   const handle = () => {
@@ -61,23 +62,53 @@ function App() {
       </div>
       <div className="px-12 pt-24 bg-blue-400">
         <div className="max-w-1087px w-full mx-auto">
-          <div className="text-center text-36px font-semibold text-white mb-24">Screenshots</div>
-          {screenShots.map((x, i) => (
-            <img src={x.url} key={`screenshot-${i}`} alt="" className="mb-24"></img>
-          ))}
-          <div className="text-center text-36px font-semibold text-white mb-16 pt-12">Features</div>
-          <div className="flex flex-wrap text-white mb-24">
+          <div className="text-center text-36px font-semibold text-white mb-16">Features</div>
+          <div className="flex flex-wrap text-white mb-12">
             {features.map((x, i) => (
-              <div
-                className="xl:w-1/3 md:w-1/2 w-full text-center block items-center mb-12"
-                key={`feature-${i}`}
-              >
-                <img src={x.image} alt="" className="mb-7 mx-auto"></img>
-                <div className="text-2xl mb-2 font-semibold">{x.title}</div>
-                <div className="font-medium max-w-208px mx-auto">{x.description}</div>
-              </div>
+              <InView threshold={0.6} triggerOnce>
+                {({ inView, ref }) => (
+                  <div
+                    ref={ref}
+                    className={`xl:w-1/3 md:w-1/2 w-full text-center block items-center transition duration-700 transform mb-24 ${
+                      !inView
+                        ? i % 3 === 0
+                          ? "-translate-x-10 opacity-0"
+                          : i % 3 === 1
+                          ? "opacity-0"
+                          : i % 3 === 2
+                          ? "translate-x-10 opacity-0"
+                          : "translate-x-0 opacity-100"
+                        : "translate-x-0 opacity-100"
+                    }`}
+                    key={`feature-${i}`}
+                  >
+                    <img src={x.image} alt="" className="mb-7 mx-auto"></img>
+                    <div className="text-2xl mb-2 font-semibold">{x.title}</div>
+                    <div className="font-medium max-w-208px mx-auto">{x.description}</div>
+                  </div>
+                )}
+              </InView>
             ))}
           </div>
+          <div className="mb-36">
+            <div className="text-center text-36px font-semibold text-white mb-24">Screenshots</div>
+            {screenShots.map((x, i) => (
+              <InView threshold={0.4} triggerOnce>
+                {({ inView, ref }) => (
+                  <img
+                    ref={ref}
+                    src={x.url}
+                    key={`screenshot-${i}`}
+                    alt=""
+                    className={`mb-24 transform duration-700 transition ${
+                      inView ? "opacity-100" : "opacity-0"
+                    }`}
+                  ></img>
+                )}
+              </InView>
+            ))}
+          </div>
+
           <Footer></Footer>
         </div>
       </div>
--- src/components/NetlifyButton.tsx
+++ src/components/NetlifyButton.tsx
@@ -3,8 +3,14 @@ import { ReactComponent as NetlifyIcon } from "assets/netlify.svg";
 export interface NetlifyButtonProps {}
 
 const NetlifyButton: React.FC<NetlifyButtonProps> = () => {
+  const handle = () => {
+    window.location.href = `https://app.netlify.com/start/deploy?repository=https://github.com/rromikas/fireck-dboard`;
+  };
   return (
-    <div className="flex items-center whitespace-nowrap text-green-netlify hover:text-white hover:bg-green-netlify h-42px rounded-lg bg-white font-medium px-5 cursor-pointer">
+    <div
+      onClick={handle}
+      className="flex items-center whitespace-nowrap text-green-netlify hover:text-white hover:bg-green-netlify h-42px rounded-lg bg-white font-medium px-5 cursor-pointer"
+    >
       <NetlifyIcon className="mr-3 fill-current"></NetlifyIcon>
       <div>Deploy to netlify</div>
     </div>
